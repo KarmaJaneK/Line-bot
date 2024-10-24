4e1f5bade91f1b9be87146d629785d0a46ab80f2
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-10-24 14:47:36 +0700

    // create LINE SDK client

--- src/lineClient.js
+++ src/lineClient.js
@@ -6,3 +6,8 @@ import express from 'express';
 const config = {
     channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
+
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient({
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+});
