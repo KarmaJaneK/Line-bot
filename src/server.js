21521739a273b0a4288e463d8f9f5c316c684695
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-10-27 10:08:32 +0700

    initiate server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const { port } = require('./config');
+
 const { handleEvent, sendReminders } = require('./handlers');
 const cron = require('node-cron');
 const { join } = require('node:path');
