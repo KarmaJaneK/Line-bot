37d2cfa7d32fb665f4439dd15cff6a5d22dea821
Karma Nel <karmajane27@gmail.com>
2024-10-30 13:40:20 +0700

    Fix: Correct LINE SDK client instantiation to resolve pushMessage method error

--- app/src/lineClient.js
+++ app/src/lineClient.js
@@ -4,14 +4,11 @@ const handleEvent = require('./handler');
 
 // create LINE SDK config from env variables
 const config = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
 // create LINE SDK client
-const client = new line.messagingApi.MessagingApiClient({
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
-});
-
+const client = new line.Client(config);
 
 module.exports = { client, line };
-
